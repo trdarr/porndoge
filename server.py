7ddd53f92f10f22cfd1d3e469672e4d7726076b1
Thomas Darr <me@trdarr.com>
2014-03-01 14:20:42 -0600

    Implement Twitter OAuth

--- server.py
+++ server.py
@@ -2,20 +2,11 @@
 
 import flask
 import operator
+import pymongo
 import random
 import re
 import requests
-
-app = flask.Flask(__name__)
-
-@app.route('/')
-def index():
-  json = requests.get('http://www.pornmd.com/getliveterms').json()
-  doge = requests.get('http://dogr.io/{}.png'.format('/'.join(get_terms(json))))
-  return flask.Response(doge.content, mimetype='image/png')
-
-if __name__ == '__main__':
-  app.run()
+import sys
 
 def get_terms(json, n=3):
   '''Get a list of n acceptable random terms from the JSON array.'''
@@ -47,3 +38,73 @@ def get_terms(json, n=3):
       random.shuffle(terms)
       return terms
 
+app = flask.Flask(__name__)
+mongo_client = pymongo.MongoClient()
+
+@app.route('/')
+def index():
+  json = requests.get('http://www.pornmd.com/getliveterms').json()
+  doge = requests.get('http://dogr.io/{}.png'.format('/'.join(get_terms(json))))
+  return flask.Response(doge.content, mimetype='image/png')
+
+@app.before_request
+def oauth_session():
+  import os
+  import requests_oauthlib as requests
+
+  key = os.environ.get('PORNDOGE_KEY')
+  secret = os.environ.get('PORNDOGE_SECRET')
+  if not key or not secret:
+    app.logger.error('OAuth client credentials not set.')
+    return flask.abort(500)
+
+  base_url = os.environ.get('PORNDOGE_BASE_URL')
+  if not base_url:
+    app.logger.error('Base URL not set.')
+    return flask.abort(500)
+
+  flask.g.session = requests.OAuth1Session(key, client_secret=secret,
+      callback_uri='{}/callback'.format(base_url.rstrip('/')))
+  flask.g.session.verify = True
+
+@app.before_request
+def db():
+  flask.g.db = mongo_client.porndoge
+
+@app.route('/oauth')
+def oauth():
+  # Get a request token.
+  url = 'https://api.twitter.com/oauth/request_token'
+  response = flask.g.session.fetch_request_token(url)
+
+  # Ask the user to authorize the request token.
+  url = 'https://api.twitter.com/oauth/authenticate'
+  return flask.redirect(flask.g.session.authorization_url(url))
+
+@app.route('/callback')
+def callback():
+  # Exchange the request token for an access token.
+  flask.g.session.parse_authorization_response(flask.request.url)
+  url = 'https://api.twitter.com/oauth/access_token'
+  tokens = flask.g.session.fetch_access_token(url)
+
+  # Verify credentials with the Twitter API.
+  url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+  response = flask.g.session.get(url)
+  if response.status_code != 200:
+    app.logger.error('Failed to verify credentials.')
+    return flask.abort(response.status_code)
+
+  # Save the user's id, screen_name, and OAuth access credentials.
+  json = response.json()
+  user = {'_id': json.get('id_str'),
+          'screen_name': json.get('screen_name'),
+          'oauth_token': tokens.get('oauth_token'),
+          'oauth_token_secret': tokens.get('oauth_token_secret')}
+  flask.g.db.users.save(user)
+
+  # TODO: Do something more interesting.
+  return flask.redirect(flask.url_for('index'))
+
+if __name__ == '__main__':
+  app.run(debug=True)
